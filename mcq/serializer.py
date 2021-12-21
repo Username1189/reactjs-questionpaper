3b3b0ab0044c43addd29c8e81bc0951a75412714
Username1183 <sanjaymuthu12@gmail.com>
2021-12-21 19:40:26 +0530

    Deploygit add .!

--- mcq/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-  
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ['question_id', 'choices', 'ques_desc', 'correct_answers']
